f6cdb8214c3ab312cf61a4530bb56becf7e7e6bb
Christian Blunt <christian.blunt12@gmail.com>
2025-11-11 17:42:39 -0500

    added cart modal that displays when clicking cart

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-const productName = document.getElementById("product-name");
-const productPrice = document.getElementById("product-price");
-const addProductBtn = document.getElementById("add-product");
+const addToCartBtns = document.querySelectorAll(".add-to-cart");
 const cart = document.getElementById("cart");
 const totalPriceS = document.getElementById("total-price");
-const cartCount = document.getElementById("cart-count");
+const cartToggle = document.getElementById("cart-toggle");
+const mobileOverlay = document.getElementById("mobile-overlay");
+const cartModal = document.getElementById("cart-modal");
+const cartOverlay = document.getElementById("cart-overlay");
+const closeCart = document.getElementById("close-cart");
 
 let totalPrice = 0;
+let cartItems = {};
+
+// Show cart when clicked
+cartToggle.addEventListener("click", () => {
+  cartModal.classList.add("show");
+  cartOverlay.classList.add("show");
+});
+
+// Hide cart when clicking the close button or outside area
+closeCart.addEventListener("click", () => {
+  cartModal.classList.remove("show");
+  cartOverlay.classList.remove("show");
+});
+
+cartOverlay.addEventListener("click", () => {
+  cartModal.classList.remove("show");
+  cartOverlay.classList.remove("show");
+});
 
 // Function to update the total price
 function updateTotalPrice(amount) {
